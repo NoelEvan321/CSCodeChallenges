cf07af7afe15937bdcafda79b47f89ba9efc8fee
NoelEvan321 <noel_evan@outlook.com>
2022-10-27 16:16:45 -0500

    finished FibSeq lab

--- CodeChallenges2/Collatz.cs
+++ CodeChallenges2/Collatz.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Collatz...The Longest Collatz");
+            //create a list of var from 13 to 1000
+            //var rangedNbrs = Enumerable.Range(13, 16-13).ToList(); to prove ranged is working the way I want it to.
+            var rangedNbrs = Enumerable.Range(13, 1_000 - 13).ToList();
+            //dictionary to store starting number in above range as key and eventual count as value
+            Dictionary<int, int> countDict = new Dictionary<int, int>();
+            //foreach to address the starting number
+            foreach (var n in rangedNbrs)
+            {
+                int newNbr = n;
+                int count = 0;
+                bool exit = false;
+
+                do
+                {
+                    if (newNbr % 2 == 0 && newNbr > 1)
+                    {
+                        newNbr = newNbr / 2;
+                        count += 1;
+                        //Console.WriteLine($"Divided by 2. Number is {newNbr}. Count is {count}");
+                    }
+                    else if (newNbr % 2 == 1 && newNbr > 1)
+                    {
+                        newNbr = (3 * newNbr) + 1;
+                        count += 1;
+                        //Console.WriteLine($"3n+1. Number is {newNbr}. Count is {count}");
+                    }
+                    else if (newNbr == 1)
+                    {
+                        count += 1;
+                        countDict.Add(n, count);
+                        //Console.WriteLine($"{newNbr} and count is {count}" );
+                        exit = true;
+                    }
+                } while (!exit);
+                //Console.WriteLine(n);
+                continue;
+            }
+            //print countDict.Max to console + key.
+            int newCount = 0;
+            int newKey = 0;
+            foreach (var (key, value) in countDict)
+            {
+                //Console.WriteLine(key);
+                //Console.WriteLine(value);
+                if (value > newCount)
+                {
+                    newCount = value;
+                    newKey = key;
+                    //Console.WriteLine($"Key:{newKey}");
+                    //Console.WriteLine($"Count:{newCount}");
+                }
+            }
+            Console.WriteLine($"Key:{newKey}");
+            Console.WriteLine($"Count:{newCount}");
         }
     }
 }
