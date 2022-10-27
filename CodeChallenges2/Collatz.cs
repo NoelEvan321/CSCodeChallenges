5ad226f2e203836360aad30d021921823639aeff
NoelEvan321 <noel_evan@outlook.com>
2022-10-27 00:02:00 -0500

    added the range adjustments

--- CodeChallenges2/Collatz.cs
+++ CodeChallenges2/Collatz.cs
@@ -6,7 +6,8 @@
         {
             Console.WriteLine("Collatz...The Longest Collatz");
             //create a list of var from 13 to 1000
-            var rangedNbrs = Enumerable.Range(13, 2).ToList();
+            //var rangedNbrs = Enumerable.Range(13, 16-13).ToList(); to prove ranged is working the way I want it to.
+            var rangedNbrs = Enumerable.Range(13, 1_000-13).ToList();
             //dictionary to store starting number in above range as key and eventual count as value
             Dictionary<int, int> countDict = new Dictionary<int, int>();
             //foreach to address the starting number
@@ -22,26 +23,26 @@
                     {
                         newNbr = newNbr / 2;
                         count+=1;
-                        Console.WriteLine($"Divided by 2. Number is {newNbr}. Count is {count}");
+                        //Console.WriteLine($"Divided by 2. Number is {newNbr}. Count is {count}");
                     }
                     else if (newNbr % 2 == 1 && newNbr > 1)
                     {
                         newNbr = (3 * newNbr) + 1;
                         count+=1;
-                        Console.WriteLine($"3n+1. Number is {newNbr}. Count is {count}");
+                        //Console.WriteLine($"3n+1. Number is {newNbr}. Count is {count}");
                     }
                     else if(newNbr == 1)
                     {
                         count+=1;
                         countDict.Add(n, count);
-                        Console.WriteLine($"{newNbr} and count is {count}" );
+                        //Console.WriteLine($"{newNbr} and count is {count}" );
                         exit = true;
                     }
                 }while(!exit);
-                Console.WriteLine(n);
+                //Console.WriteLine(n);
                 continue;
             }
-            //dowhile to run through if tree.
+            //print countDict.Max to console + key.
 
         }
     }
