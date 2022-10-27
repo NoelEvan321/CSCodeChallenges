e6c8ae1a368217f762d84bcc36f7b304af9f3f50
NoelEvan321 <noel_evan@outlook.com>
2022-10-26 23:07:35 -0500

    Created the CodeChallenges2 project. Added the three classes.

---
+++ CodeChallenges2/FibSequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges2
+{
+    internal class FibSequence
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
