a842adc34017827d3b6310e245aa992e1a63f9d3
NoelEvan321 <noel_evan@outlook.com>
2022-10-26 23:50:43 -0500

    updated collatz. Adjusted range count.

--- CodeChallenges2/Collatz.cs
+++ CodeChallenges2/Collatz.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Collatz...The Longest Collatz");
+            //create a list of var from 13 to 1000
+            var rangedNbrs = Enumerable.Range(13, 2).ToList();
+            //dictionary to store starting number in above range as key and eventual count as value
+            Dictionary<int, int> countDict = new Dictionary<int, int>();
+            //foreach to address the starting number
+            foreach(var n in rangedNbrs)
+            {
+                int newNbr = n;
+                int count = 0;
+                bool exit = false;
+
+                do
+                {
+                    if (newNbr % 2 == 0 && newNbr > 1)
+                    {
+                        newNbr = newNbr / 2;
+                        count+=1;
+                        Console.WriteLine($"Divided by 2. Number is {newNbr}. Count is {count}");
+                    }
+                    else if (newNbr % 2 == 1 && newNbr > 1)
+                    {
+                        newNbr = (3 * newNbr) + 1;
+                        count+=1;
+                        Console.WriteLine($"3n+1. Number is {newNbr}. Count is {count}");
+                    }
+                    else if(newNbr == 1)
+                    {
+                        count+=1;
+                        countDict.Add(n, count);
+                        Console.WriteLine($"{newNbr} and count is {count}" );
+                        exit = true;
+                    }
+                }while(!exit);
+                Console.WriteLine(n);
+                continue;
+            }
+            //dowhile to run through if tree.
+
         }
     }
 }
