4a1a9a27662e147d1a49cbf39642507aa6cd2ee3
NoelEvan321 <noel_evan@outlook.com>
2022-10-27 16:43:25 -0500

    Finished the fibseq10 lab

--- CodeChallenges2/FibSequenceTenDigit.cs
+++ CodeChallenges2/FibSequenceTenDigit.cs
@@ -10,7 +10,31 @@ namespace CodeChallenges2
     {
         static void Main(string[] args)
         {
+            
+            bool exit = false;
+            List<long> fibSeq = new List<long>() { 1, 2 };
+            long startNbr = 1;
+            long fibNbr = startNbr;
+            int digitNumber = fibNbr.ToString().Length;
+            int i = 0;//so that when we hit 10 digits we account for the index of the 10 digit number.
 
+            do
+            {
+
+                if (digitNumber < 10 )
+                {
+                    fibNbr = fibSeq[fibSeq.Count - 1] + fibSeq[fibSeq.Count - 2];
+                    fibSeq.Add(fibNbr);
+                    digitNumber = fibNbr.ToString().Length;
+                    i+=1;
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    exit = true;
+                }
+            } while (!exit);
+            Console.WriteLine(i);
         }
     }
 }
